3d3e78d43ceddb872dad50df40e65f5cb9ebda22
Leonardo200298 <leonardosanchez119@gmail.com>
2024-10-01 17:25:32 -0300

    pruebas para carga

---
+++ main/carga.js
@@ -0,0 +1,14 @@
+/* Pruebas para carga */
+
+/* setInterval(() => {
+    let numDom = document.querySelector("#carga")
+    numDom.innerHTML = 20;
+    numDom.innerHTML = 50;
+    numDom.innerHTML = 70;
+    numDom.innerHTML = 73;
+    numDom.innerHTML = 73.2;
+    numDom.innerHTML = 73.5;
+    numDom.innerHTML = 73.7;
+    numDom.innerHTML = "facu puto";
+    numDom.innerHTML = 100;
+}, 3000); */
