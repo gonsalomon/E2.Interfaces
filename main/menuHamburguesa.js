7109c003ba23b0666b5c7952fb50e73112dda29a
Leonardo200298 <leonardosanchez119@gmail.com>
2024-10-01 16:53:23 -0300

    agrego mejora en menu de hamburguesa conveniente a los estilos anteriormente realizados

--- main/menuHamburguesa.js
+++ main/menuHamburguesa.js
@@ -1,4 +1,5 @@
 let boton = document.querySelector("#boton");
+let espacio = document.querySelector("#espacio");
 let menu = document.querySelector("#menu");
 let closeBtn = document.querySelector("#close-btn");
 
@@ -9,10 +10,11 @@ function abrirMenu(event) {
     event.preventDefault();
     menu.classList.remove("oculto");  
     boton.style.display = "none"; 
-   
+    espacio.style.display = "flex"
 }
 
 function cerrarMenu() {
     menu.classList.add("oculto");     
     boton.style.display = "flex";     // Muestra nuevamente el botón de hamburguesa aplicando flex
+    espacio.style.display = "none";
 }
