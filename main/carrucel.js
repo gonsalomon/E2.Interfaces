d39b9f443a795b3d976bf8fb2343d553c36474dd
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2024-10-04 18:51:41 -0300

    finalizacion del carrucel, integracion del headery footer a la pagina principal

--- main/carrucel.js
+++ main/carrucel.js
@@ -1,19 +1,33 @@
-let incremetn = 0 ;
-let btn_izq = document.querySelector(".btn_btn_carrucel_izq");
-let btn_der = document.querySelector(".btn_btn_carrucel_der");
-let card = document.querySelector(".card")
+let pos = 0;
+let increment = 0 ;
+let btn_izq = document.querySelector(".btn_carrucel_izq");
+let btn_der = document.querySelector(".btn_carrucel_der");
+let cards = document.querySelectorAll(".card")
 
-btn_izq.addEventListener("click", escrolDer);
+btn_der.addEventListener("click", escrolDer);
 btn_izq.addEventListener("click", escrolIzq);
+
 
 function escrolIzq(event){
     event.preventDefault();
-    card.style.animationName="animacion_card_izq"
-    card.style.transform = "translate(150px)";
+    if(pos > 0){ 
+        increment+= 150 
+        pos--
+    } 
+    cards.forEach((card) => {
+        card.style.animationName = "animacionCardIzq"; 
+        card.style.transform = "translate(" + increment +"px)";
+    });   
 }
 
 function escrolDer(event){
     event.preventDefault();
-    card.style.animationName="animacion_card_der"
-    card.style.transform = "translate(-150px)";
+    if(pos <= cards.length-1){
+        increment -= 150
+        pos ++
+    }
+    cards.forEach((card) => {
+        card.style.animationName = "animacionCardDer";
+        card.style.transform = "translate("+ increment +"px)";
+    });
 }
