69a6038cd0dc19e8ec684f8c00f9b919f7fb4926
Leonardo200298 <leonardosanchez119@gmail.com>
2024-10-10 19:39:39 -0300

    implementacion de logica para formularios, mejora en estilos en diseños mobile first, faltan cambiar headers y footers de demas paginas

--- main/menuHamburguesa.js
+++ main/menuHamburguesa.js
@@ -1,24 +1,10 @@
 let boton = document.querySelector("#boton");
 let espacio = document.querySelector("#espacio");
 let menu = document.querySelector("#menu");
-let contenedorDeFormulario = document.querySelector("#contenedorDeFormulario");
 let closeBtn = document.querySelector("#close-btn");
-let espacioDeFormularioEnviado = document.querySelector("#parte-de-exito");
-let botonFormulario = document.querySelector("#botonFormulario");
-let estadoDeSesion = document.querySelector("#estadoDeSesion")
 
-botonFormulario.addEventListener("click", esconderFormulario);
 boton.addEventListener("click", abrirMenu);
 closeBtn.addEventListener("click", cerrarMenu);
-
-function esconderFormulario(e){
-    e.preventDefault();
-   
-    espacioDeFormularioEnviado.classList.remove("oculto");
-    espacioDeFormularioEnviado.classList.add("contenedor-exitos");
-    estadoDeSesion.innerHTML = "Cerrar sesion";
-
-}
 
 
 function abrirMenu(event) {
