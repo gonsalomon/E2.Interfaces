4ab815cd1dfa3aa312b4ba4bb9164e149ad621d2
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2024-10-11 15:22:14 -0300

    correji defecto en los carruceles cambando un poco el carrucel.js

--- main/carrucel.js
+++ main/carrucel.js
@@ -1,33 +1,66 @@
 let pos = 0;
 let increment = 0 ;
+
+/* carrucel 1 */
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 let btn_izq = document.querySelector("#botonCarrouselIzq");
 let btn_der = document.querySelector("#botonCarrouselDer");
 let btn_der_flecha= document.querySelector("#fechaDer");
 let btn_izq_flecha= document.querySelector("#fechaIzq");
+let cards = document.querySelectorAll("#card");
+
+/* carrucel 2 */
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 let btn_izq2 = document.querySelector("#botonCarrouselIzq2");
 let btn_der2 = document.querySelector("#botonCarrouselDer2");
 let btn_der_flecha2= document.querySelector("#fechaDer2");
 let btn_izq_flecha2= document.querySelector("#fechaIzq2");
+let cards2 = document.querySelectorAll("#card2");
+
+
+/* carruel 3 */
+/* """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" */
 let btn_izq3 = document.querySelector("#botonCarrouselIzq3");
 let btn_der3 = document.querySelector("#botonCarrouselDer3");
 let btn_der_flecha3= document.querySelector("#fechaDer3");
 let btn_izq_flecha3= document.querySelector("#fechaIzq3");
-let cards = document.querySelectorAll(".card");
+let cards3 = document.querySelectorAll("#card3");
 
+
+
+/* llamdada carrucel 1 */
+/*  1111111111111111111111111111111111111111111111111111111111111*/
 btn_der.addEventListener("click", escrolDer);
 btn_izq.addEventListener("click", escrolIzq);
 btn_der_flecha.addEventListener("click",escrolDer);
 btn_izq_flecha.addEventListener("click",escrolIzq);
-btn_der2.addEventListener("click", escrolDer);
-btn_izq2.addEventListener("click", escrolIzq);
-btn_der_flecha2.addEventListener("click",escrolDer);
-btn_izq_flecha2.addEventListener("click",escrolIzq);
-btn_der3.addEventListener("click", escrolDer);
-btn_izq3.addEventListener("click", escrolIzq);
-btn_der_flecha3.addEventListener("click",escrolDer);
-btn_izq_flecha3.addEventListener("click",escrolIzq);
 
-function escrolIzq(event){
+
+/* llamada carruvel 2 */
+/*  2222222222222222222222222222222222222222222222222222222222222222*/
+btn_der2.addEventListener("click", escrolDer2);
+btn_izq2.addEventListener("click", escrolIzq2);
+btn_der_flecha2.addEventListener("click",escrolDer2);
+btn_izq_flecha2.addEventListener("click",escrolIzq2);
+
+
+
+/* llamada carrucel 3 */
+/* 33333333333333333333333333333333333333333333333333333333333333 */
+btn_der3.addEventListener("click", escrolDer3);
+btn_izq3.addEventListener("click", escrolIzq3);
+btn_der_flecha3.addEventListener("click",escrolDer3);
+btn_izq_flecha3.addEventListener("click",escrolIzq3);
+
+
+
+
+
+
+
+/* funciones */
+
+function escrolIzq(event){/* crea el escrol al boron izquierdo */
     event.preventDefault();
     if(pos > 0){ 
         increment+= 150 
@@ -39,7 +72,7 @@ function escrolIzq(event){
     });   
 }
 
-function escrolDer(event){
+function escrolDer(event){ /* crea el scroll para el boron derecho */
     event.preventDefault();
     if(pos <= cards.length-1){
         increment -= 150
@@ -50,4 +83,60 @@ function escrolDer(event){
         card.style.transform = "translate("+ increment +"px)";
     });
 }
-/* Segundo carrusel */
+
+
+/* funciones carrucel 2 */
+/* 222222222222222222222222222222222222222222222222222222222222222222 */
+function escrolIzq2(event){/* crea el escrol al boron izquierdo */
+    event.preventDefault();
+    if(pos > 0){ 
+        increment+= 150 
+        pos--
+    } 
+    cards2.forEach((card) => {
+        card.style.animationName = "animacionCardIzq"; 
+        card.style.transform = "translate(" + increment +"px)";
+    });   
+}
+
+function escrolDer2(event){ /* crea el scroll para el boron derecho */
+    event.preventDefault();
+    if(pos <= cards.length-1){
+        increment -= 150
+        pos ++
+    }
+    cards2.forEach((card) => {
+        card.style.animationName = "animacionCardDer";
+        card.style.transform = "translate("+ increment +"px)";
+    });
+}
+
+
+
+
+/* funciones carrucel 3 */
+/* 333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 */
+function escrolIzq3(event){/* crea el escrol al boron izquierdo */
+    event.preventDefault();
+    if(pos > 0){ 
+        increment+= 150 
+        pos--
+    } 
+    cards3.forEach((card) => {
+        card.style.animationName = "animacionCardIzq"; 
+        card.style.transform = "translate(" + increment +"px)";
+    });   
+}
+
+function escrolDer3(event){ /* crea el scroll para el boron derecho */
+    event.preventDefault();
+    if(pos <= cards.length-1){
+        increment -= 150
+        pos ++
+    }
+    cards3.forEach((card) => {
+        card.style.animationName = "animacionCardDer";
+        card.style.transform = "translate("+ increment +"px)";
+    });
+}
+
